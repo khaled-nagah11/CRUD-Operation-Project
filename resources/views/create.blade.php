54e8783c4373fd3b4a9486d32361da71f5538eef
khaled <kaled21@std.mans.edu.eg>
2024-01-29 19:14:43 +0200

    validation error (alert massege)

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,6 +8,15 @@
 
 @section('content')
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="post" action="{{route('posts.store')}}">
         @csrf
         <div class="mb-3">
